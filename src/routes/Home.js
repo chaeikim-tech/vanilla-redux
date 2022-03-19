11dc69c54313a621d6b0ece0969d5a18f652dff4
eunju.kim <sb02348@hanmail.net>
2022-03-20 03:45:13 +0900

    Using react-redux connect-mapstate

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import { connect } from 'react-redux';
 
-function Home() {
+function Home({ toDos }) {
   const [text, setText] = useState("");
   function onChange(event) {
     setText(event.target.value);
@@ -16,9 +17,15 @@ function Home() {
         <input type="text" value={text} onChange={onChange} />
         <button>Add</button>
       </form>
-      <ul></ul>
+      <ul>{JSON.stringify(toDos)}</ul>
     </>
   );
 }
 
-export default Home;
+function mapStateToProps(state){
+    return { toDos: state};
+}
+
+export default connect(mapStateToProps)(Home);
+// connect = components들을 store에 연결해줌.
+// https://react-redux.js.org/using-react-redux/connect-mapstate
